df1b99e66e6e837cebaaa5daeee1e4952179da39
noah milliken <noahmilliken@gmail.com>
2022-06-15 21:42:45 -0400

    made a gamegit add .git add .git add .

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let boobs = "fun";
-let nipples = 'pointy';
-const bros = 'boys';
 
-if(nipples !== 'pointy'){
-    console.log('useless')
-}else{
-    console.log('splendid')
+function calculateSpread(firstTeam = 'team 1', secondTeam = 'team 2', firstScore = 10, secondScore = 7, spread = 2 , ChoseFirstTeam = true){
+    // team1 score 20 spread 6.5 team2 score 35
+    let spreadResult = firstScore - secondScore
+    if(spreadResult >= spread && ChoseFirstTeam){
+        return `You chose ${firstTeam} and they beat the spread by ${spreadResult}!`
+    }
+        return `You chose ${firstTeam} and they got beat by a spread of ${spreadResult}!`
+
+
 }
+
+console.log(calculateSpread('Detroit','Green Bay', 15, 12, 1, true))
+
+
