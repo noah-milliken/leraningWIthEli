6d68e231e15c53f9bc233bed2294347d017c6be9
noah milliken <noahmilliken@gmail.com>
2022-06-19 17:44:50 -0400

    some troubleshooting

--- index.js
+++ index.js
@@ -14,3 +14,67 @@ console.log(calculateSpread('Detroit','Green Bay', 15, 12, 1, true))
 
 
 console.log(calculateSpread('miami', 'buffalo', 56, 23, 6.5, false))
+
+console.log(calculateSpread())
+
+function calculateMileage(miles = 0, rate = 0){
+    let cost = miles * rate
+    return cost
+}
+console.log(calculateMileage(68, 0.56))
+
+function gpaCalc(gpa = 0, ){
+
+}
+
+function multiply(a,b){
+    console.log(a*b)
+}
+multiply(599,44)
+
+function calcBMI(height, weight){
+    let BMI = weight * (height^2)
+    console.log(BMI)
+
+}calcBMI(188, 210)
+
+
+const array = [78,56,232,412,228]
+array1 = array.sort((a,b)=> a-b)
+console.log(array1)
+
+
+let banana = 'banana '
+
+banana= banana.repeat(3)
+
+console.log(banana)
+
+let arr1 = [1,2,3]
+let arr2 = [2,4,6]
+
+console.log(...arr1)
+
+
+function pushElement(arr){
+    var el=4;
+    //push el to arr
+    console.log(arr.push(el))
+  }
+  pushElement([1,2,3,])
+
+
+
+  const circle = {
+    radius: 1,
+    location:{
+        x:1,
+        y: 1
+    },
+    draw: function(){
+        console.log(draw);
+    }
+  }
+
+  circle.draw();
+
